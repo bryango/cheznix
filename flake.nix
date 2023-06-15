9c3d40a08dd8bb5519d3e3b8653ed3378368677f
Bryan Lai <bryanlais@gmail.com>
2023-06-15 22:36:16 +0800

    Restructure flake for multiple output attributes

--- flake.nix
+++ flake.nix
@@ -3,64 +3,63 @@
 
   inputs = {
 
-    attrs = {
-      url = "git+ssh://git@github.com/bryango/attrs.git";
-      ## ... provides attrset with `system`, `username`, `homeDirectory`, ...
-      ## ... WARNING: not secret, might leak through /nix/store & cache
-    };
+    ## specify system attributes: for each system,
+    ## ... provide attrsets with `system`, `username`, `homeDirectory`, ...
+    ## ... WARNING: not secret, might leak through /nix/store & cache
+    all-attrs.url = "git+ssh://git@github.com/bryango/attrs.git";
 
-    ## Specify the source of Home Manager and Nixpkgs.
+    ## specify the source of home-manager and nixpkgs
     ## ... using flake registry:
     nixpkgs.url = "nixpkgs";
 
     ## alternatively,
+    # nixpkgs.url = "nixpkgs/a3a3dda3bacf61e8a39258a0ed9c924eeca8e293";
     # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     ## ... `unstable` lags a little bit behind `master`
-    ## ... one may also specify a commit:
-    # nixpkgs.url = "nixpkgs/a3a3dda3bacf61e8a39258a0ed9c924eeca8e293";
 
     home-manager = {
       url = "home-manager";
       # url = "github:nix-community/home-manager";
 
-      ## ... home-manager is also a flake
+      ## home-manager is also a flake
       ## ... but we ask it to follow our nixpkgs:
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
 
-  outputs = { nixpkgs, home-manager, attrs, ... }:
+  outputs = { nixpkgs, home-manager, all-attrs, ... }:
     let
-      machine = "btrsamsung";
-      attrs' = attrs.${machine} // {
-        config = {
-          ## https://github.com/nix-community/home-manager/issues/2954
-          ## ... home-manager/issues/2942#issuecomment-1378627909
-          allowBroken = true;
-          allowUnfree = true;
+      mkMachineConfig = hostname:
+        let
+          attrs = all-attrs.${hostname} // {
+            config = {
+              ## https://github.com/nix-community/home-manager/issues/2954
+              ## ... home-manager/issues/2942#issuecomment-1378627909
+              allowBroken = true;
+              allowUnfree = true;
+            };
+          };
+          pkgs = import nixpkgs {
+            inherit (attrs) system config;
+          };
+        in {
+          homeConfigurations."${attrs.username}@${hostname}" =
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [ ./home.nix ];
+
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+            extraSpecialArgs = {
+              inherit attrs;
+            };
+          };
         };
-      };
-    in
-    let
-      attrs = attrs';
-      pkgs = import nixpkgs {
-        inherit (attrs) system config;
-      };
     in {
-      homeConfigurations."${attrs.username}@${machine}" =
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-        extraSpecialArgs = {
-          inherit attrs;
-        };
-      };
+      inherit (mkMachineConfig "btrsamsung") homeConfigurations;
     };
 }
