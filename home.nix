e2b5c6652cfbb0dc2ec5bf98438ac82edffb1b3c
Bryan Lai <bryanlais@gmail.com>
2023-06-19 16:52:33 +0800

    Try to fix fcitx5-configtool w/ nativeBuildInputs

--- home.nix
+++ home.nix
@@ -42,6 +42,9 @@
   nixpkgs.config = {
     packageOverrides = pkgs: {
       ## home overrides
+      fcitx5-configtool = pkgs.libsForQt5.callPackage ./fcitx5-configtool.nix {
+        kcmSupport = false;
+      };
     };
   };
 
