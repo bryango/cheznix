495498f1b3c9daa8060dced7a4db1a2e3cc16c17
Bryan Lai <bryanlais@gmail.com>
2023-06-25 21:43:27 +0800

    Re-organize home.nix for better access

--- home.nix
+++ home.nix
@@ -38,15 +38,14 @@
     gnomeExtensions.caffeine
     gnomeExtensions.kimpanel
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
+    ## It is sometimes useful to fine-tune packages, for example, by applying
+    ## overrides. You can do that directly here, just don't forget the
+    ## parentheses:
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
+    ## You can also create simple shell scripts directly inside your
+    ## configuration. For example, this adds a command 'my-hello' to your
+    ## environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
@@ -68,62 +67,6 @@
     };
   };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/bryan/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-
-    ## use system locale; see `disabledModules`
-    LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  ## use system manpage
-  programs.man.enable = false;
-
-  ## modules overrides
-  disabledModules = [
-
-    ## https://github.com/nix-community/home-manager/issues/2333
-    ## https://github.com/nix-community/home-manager/blob/master/modules/config/i18n.nix
-    ## use system locale; see `sessionVariables`
-    "config/i18n.nix"
-
-  ];
 
   imports = [
     ./redshift-many
@@ -149,14 +92,17 @@
     };
   };
 
-  ## does not work well with non-nix host
-  # i18n.inputMethod = {
-  #   enabled = "fcitx5";
-  #   fcitx5.addons = with pkgs; [
-  #     ## https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/inputmethods/fcitx5/with-addons.nix
-  #     fcitx5-chinese-addons
-  #   ];
-  # };
+  ## use system manpage
+  programs.man.enable = false;
+
+  disabledModules = [
+
+    ## https://github.com/nix-community/home-manager/issues/2333
+    ## https://github.com/nix-community/home-manager/blob/master/modules/config/i18n.nix
+    ## use system locale; see `sessionVariables`
+    "config/i18n.nix"
+
+  ];
 
   ## nix settings
   nix.package = pkgs.nix;
@@ -173,7 +119,61 @@
     ## ... see also: man nix.conf
     ## ... https://nixos.org/manual/nix/stable/#sec-conf-file
 
-    # vim: set ft=nix:'';
+    # vim: set ft=nix:''
+  ;
+
+  home.file = {
+    ## Building this configuration will create a copy of 'dotfiles/screenrc' in
+    ## the Nix store. Activating the configuration will then make '~/.screenrc' a
+    ## symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    ## You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/bryan/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+
+    ## use system locale; see `disabledModules`
+    LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+  };
+
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "22.11"; # Please read the comment before changing.
+
+  ## let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  ## does not work well with non-nix host
+  # i18n.inputMethod = {
+  #   enabled = "fcitx5";
+  #   fcitx5.addons = with pkgs; [
+  #     ## https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/inputmethods/fcitx5/with-addons.nix
+  #     fcitx5-chinese-addons
+  #   ];
+  # };
 
   ## ~/.config/nix/registry.json
   ## ... pin here if don't want to update
@@ -191,4 +191,5 @@
   #   };
 
   # };
+
 }
