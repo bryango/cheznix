63dc1bc5562d798e02a735ff3a54b98dfc31599a
Bryan Lai <bryanlais@gmail.com>
2023-06-26 17:59:16 +0800

    Fix binary-fallback bug

--- pkgs/binary-fallback/binary-fallback.sh
+++ pkgs/binary-fallback/binary-fallback.sh
@@ -1,11 +1,14 @@
+fullname=$(readlink -f "$0")
+
 system_binary=$(
     which -a "@name@" \
-    | grep -v "$0" \
+    | xargs readlink -f \
+    | grep -v "$fullname" \
     | head -1
 )
 
 if [[ -x $system_binary ]]; then
     exec "$system_binary" "$@"
 else
-    exec "@package@" "$@"
+    exec "@package@/bin/@name@" "$@"
 fi
