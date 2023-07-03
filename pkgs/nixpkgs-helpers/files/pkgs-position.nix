c1783a5873850ba8f9b47f3f678e210c579151f6
Bryan Lai <bryanlais@gmail.com>
2023-07-03 20:11:13 +0800

    Make `pkgs-position` more robust

--- pkgs/nixpkgs-helpers/files/pkgs-position.nix
+++ pkgs/nixpkgs-helpers/files/pkgs-position.nix
@@ -6,12 +6,13 @@
 
 let
 
-  ## so we use environment variables:
-  package = builtins.getEnv "package";
-
   ## load nixpkgs (need to be set up first)
   pkgs = import <nixpkgs> { };
   lib = pkgs.lib;
+
+  ## get environment variables:
+  packageAttrPath = lib.splitString "." (builtins.getEnv "package");
+  package = lib.getAttrFromPath packageAttrPath pkgs;
 
   ## human readable nixpkgs path,
   ## under e.g. ~/.nix-defexpr/channels
@@ -19,7 +20,7 @@ let
   storePath = toString pkgs.path;
   ## ^ nixpkgs /nix/store path
 
-  storePosition = pkgs.${package}.meta.position;
+  storePosition = package.meta.position;
   basePosition = lib.removePrefix storePath storePosition;
 
   nicePosition = toString (/. + nixpkgsPath + basePosition);
