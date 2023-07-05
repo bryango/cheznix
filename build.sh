945e7b8839489256dd4b447fc76dc25982ecf39b
Bryan Lai <bryanlais@gmail.com>
2023-07-05 23:03:17 +0800

    Update nixpkgs-config & build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -x
 cd "$(dirname "$0")" || exit
 
 IFS=" " read -r -a allConfigs <<< "$(nix eval --raw --impure --expr \
-  "with builtins; toString (attrNames (getFlake path:$PWD).homeConfigurations)" \
+  "with builtins; attrNames (getFlake path:$PWD).homeConfigurations" \
+  --apply toString \
   | xargs
 )"
 
