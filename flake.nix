f1511c06bc0230c8b2c1457cd622ba71df580604
Bryan Lai <bryanlais@gmail.com>
2023-07-06 00:09:55 +0800

    Fix infinite recursion from the gathered overlay

--- flake.nix
+++ flake.nix
@@ -126,20 +126,16 @@
     } ## then we expose some subpackges:
     // hostSymlinks;
 
+    userOverlaid = "user-overlaid";
     gatherOverlaid = system: final: prev: let
 
       overlaid = genOverlay system final prev;
       derivable = lib.filterAttrs (name: lib.isDerivation) overlaid;
-
-      userOverlaid = "user-overlaid";
       inherit (prev) linkFarm;
 
     in {
       ${userOverlaid} = linkFarm userOverlaid derivable;
     };
-
-
-  in {
 
     legacyPackages = forMySystems (system: import nixpkgs {
 
@@ -153,8 +149,18 @@
 
       };
 
-      overlays = [ (genOverlay system) (gatherOverlaid system) ];
+      overlays = [ (genOverlay system) ];
     });
+
+  in {
+
+    inherit legacyPackages;
+
+    packages = forMySystems (
+      system: lib.genAttrs [ userOverlaid ] (
+        legacyPackages.${system}.extend (gatherOverlaid system)
+      )
+    );
 
     overlays = forMySystems genOverlay;
 
