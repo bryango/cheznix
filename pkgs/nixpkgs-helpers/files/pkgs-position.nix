d69d8dc668e53d3b33591ec7defb6c663c85fc72
Bryan Lai <bryanlais@gmail.com>
2023-07-09 13:34:14 +0800

    Revamp pkgs-position.nix

--- pkgs/nixpkgs-helpers/files/pkgs-position.nix
+++ pkgs/nixpkgs-helpers/files/pkgs-position.nix
@@ -1,26 +1,53 @@
 #!/usr/bin/env -S nix eval --impure --file
 # given env `$package`, find its definition in <nixpkgs>
 
-## `nix eval` does not work with `--arg`
-## https://github.com/NixOS/nix/issues/2678
-
 let
 
-  ## load nixpkgs (need to be set up first)
+  /* `nix eval` does not work with `--arg`, so we use environment variables.
+      See: https://github.com/NixOS/nix/issues/2678
+  */
+  attrPath = lib.splitString "." (builtins.getEnv "package");
+
+  /* load <nixpkgs>, need to be set up first!
+
+    - Why channel? Because channels live in a human-readable, _constant_
+      location such as `~/.nix-defexpr/channels`.
+
+    - On the other hand, a flake always evaluate "in-store", due to its
+      purity. So there is no way to get a nice location unless supplied
+      explicitly by the user.
+  */
   pkgs = import <nixpkgs> { };
   lib = pkgs.lib;
 
-  ## get environment variables:
-  packageAttrPath = lib.splitString "." (builtins.getEnv "package");
-  package = lib.getAttrFromPath packageAttrPath pkgs;
+  nixpkgsPath = toString <nixpkgs>;  ## human readable nixpkgs path
+  storePath = toString pkgs.path;  ## nixpkgs /nix/store path
 
-  ## human readable nixpkgs path,
-  ## under e.g. ~/.nix-defexpr/channels
-  nixpkgsPath = toString <nixpkgs>;
-  storePath = toString pkgs.path;
-  ## ^ nixpkgs /nix/store path
+  attrHost = let
+      findAttrHost = lib.findFirst (lib.hasAttrByPath attrPath) {};
+    in findAttrHost [
+      pkgs
+      lib
+    ]; ## ^ find attrPath in the above attr "hosts"
 
-  storePosition = package.meta.position;
+  package = lib.getAttrFromPath attrPath attrHost;
+
+  shortAttr = lib.last attrPath;
+  parentAttr = let
+      dropLast = list: with builtins;
+        genList (i: elemAt list i) ((length list) - 1);
+      parentPath = dropLast attrPath;
+    in lib.getAttrFromPath parentPath attrHost;
+
+  ## pkgs/stdenv/generic/make-derivation.nix
+  ## pkgs/stdenv/generic/check-meta.nix
+  getAttrPos = attrname: attrset: let
+      pos = builtins.unsafeGetAttrPos attrname attrset;
+    in "${pos.file}:${toString pos.line}:${toString pos.column}";
+
+  storePosition = package.meta.position
+    or (getAttrPos shortAttr parentAttr);
+
   basePosition = lib.removePrefix storePath storePosition;
 
   nicePosition = toString (/. + nixpkgsPath + basePosition);
