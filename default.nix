0f65b9a08fa4a77e75fc8d19fda5a07e5e3aef08
Bryan Lai <bryanlais@gmail.com>
2023-07-11 01:49:51 +0800

    Improve args passing to upstream nixpkgs

--- default.nix
+++ default.nix
@@ -1,24 +1,38 @@
-{ system ? builtins.currentSystem or "x86_64-linux", ... } @ args_:
+{ flakeSystem ? args_.system
+    or builtins.currentSystem
+    or "x86_64-linux"
+, ...
+} @ args_:
 
 let
   pathString = builtins.toString ./.;
   flake = builtins.getFlake pathString;
   nixpkgs = flake.inputs.nixpkgs;
   lib = flake.lib;
-  pkgs = flake.legacyPackages.${system};
+  pkgs = flake.legacyPackages.${flakeSystem};
 
   /* prepare the arguments for the `nixpkgs` function
   
-    Caveats:
-    - the "*ackageOverrides" will be overriden, not merged
-    - only legacy "system" is supported, not "localSystem"
+    Note:
+    - "config.*ackageOverrides" will be overriden, not merged
+    - "overlays" are composed by stacking them together
+    - see e.g. home-manager|nixos: modules/misc/nixpkgs.nix
   */
-  args = lib.recursiveUpdate {
-    inherit (pkgs)
-      config
-      overlays
-    ;
-  } args_;
+  overlays =
+    (pkgs.overlays or [ ]) ++
+    (args_.overlays or [ ]);
+
+  args__ = builtins.removeAttrs args_ [
+    "flakeSystem"
+    "overlays"
+  ];  ## ^ remove processed args
+
+  args = lib.recursiveUpdate
+    {
+      inherit (pkgs) config;
+      inherit overlays;
+    }
+    args__;
 
 in import nixpkgs args // {
   inherit flake;
