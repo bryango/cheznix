ea2405026f5e25a875d02018594a630f11add7fd
Bryan Lai <bryanlais@gmail.com>
2023-07-10 11:05:20 +0800

    Do import nixpkgs in default.nix
    This allows passing additional nixpkgs.config

--- default.nix
+++ default.nix
@@ -1,6 +1,25 @@
-{ system ? builtins.currentSystem or "x86_64-linux", ... }:
+{ system ? builtins.currentSystem or "x86_64-linux", ... } @ args_:
 
 let
   pathString = builtins.toString ./.;
   flake = builtins.getFlake pathString;
-in { inherit flake; } // flake // flake.legacyPackages.${system}
+  nixpkgs = flake.inputs.nixpkgs;
+  lib = flake.lib;
+  pkgs = flake.legacyPackages.${system};
+
+  /* prepare the arguments for the `nixpkgs` function
+  
+    Caveats:
+    - the "*ackageOverrides" will be overriden, not merged
+    - only legacy "system" is supported, not "localSystem"
+  */
+  args = lib.recursiveUpdate {
+    inherit (pkgs)
+      config
+      overlays
+    ;
+  } args_;
+
+in import nixpkgs args // {
+  inherit flake;
+}
--- pkgs/tectonic-with-biber.nix
+++ pkgs/tectonic-with-biber.nix
@@ -20,7 +20,6 @@
       }
 */
 
-
 { lib
 , makeBinaryWrapper
 , symlinkJoin
