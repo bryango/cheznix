d5f05d74e2ab4f995ae157df74b0a7fd8070b43e
Bryan Lai <bryanlais@gmail.com>
2023-07-11 23:22:42 +0800

    Use `longDescription` for `tectonic-with-biber`

--- pkgs/tectonic-with-biber.nix
+++ pkgs/tectonic-with-biber.nix
@@ -1,25 +1,3 @@
-/* wrap tectonic with biber without triggering rebuilds
-
-  This serves as a fix for:
-  - https://github.com/NixOS/nixpkgs/issues/88067
-  - https://github.com/tectonic-typesetting/tectonic/issues/893
-
-  Example use:
-
-    ## last successful build of e.g. biber-2.17
-    ## ... from https://hydra.nixos.org/build/202359527
-    let
-      rev = "40f79f003b6377bd2f4ed4027dde1f8f922995dd";
-      nixpkgs_biber217 = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-        sha256 = "1javsbaxf04fjygyp5b9c9hb9dkh5gb4m4h9gf9gvqlanlnms4n5";
-      }) {};
-    in
-      tectonic-with-biber.override {
-        biber = nixpkgs_biber217.biber;
-      }
-*/
-
 { lib
 , makeBinaryWrapper
 , symlinkJoin
@@ -31,17 +9,43 @@ let
 
   pname = "tectonic-with-biber";
   inherit (biber) version;
+
+  ## manually construct the name for symlinkJoin
   name = "${pname}-${version}";
 
   meta = tectonic.meta // {
     inherit name;
     description = "Modernized TeX/LaTeX engine, with biber for bibliography";
+    longDescription = ''
+      This package wraps tectonic with biber without triggering rebuilds.
+      The biber executable is exposed with a version suffix, such as
+      `biber-2.17`, to prevent conflict with the `biber` associated with
+      the texlive bundles. Example use:
+
+          ## pin last successful build of e.g. biber-2.17
+          ## ... from https://hydra.nixos.org/build/202359527
+          let
+            rev = "40f79f003b6377bd2f4ed4027dde1f8f922995dd";
+            nixpkgs_biber217 = import (builtins.fetchTarball {
+              url = "https://github.com/NixOS/nixpkgs/archive/''${rev}.tar.gz";
+              sha256 = "1javsbaxf04fjygyp5b9c9hb9dkh5gb4m4h9gf9gvqlanlnms4n5";
+            }) {};
+          in
+            tectonic-with-biber.override {
+              biber = nixpkgs_biber217.biber;
+            }
+
+      This serves as a fix for:
+      - https://github.com/NixOS/nixpkgs/issues/88067
+      - https://github.com/tectonic-typesetting/tectonic/issues/893
+    '';
   };
 
   ## produce the correct `meta.position`
   pos = builtins.unsafeGetAttrPos "description" meta;
 
 in
+
 symlinkJoin {
 
   inherit pname version name meta pos;
