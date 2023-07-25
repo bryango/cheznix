db9134199679ee5b9e922eb00228769fe502c26d
Bryan Lai <bryanlais@gmail.com>
2023-07-25 13:34:04 +0800

    Add version attr for biber217

--- flake.nix
+++ flake.nix
@@ -49,21 +49,24 @@
         inherit system config;
       };
 
-      biber217 = builtins.fetchClosure {
-        /* experimental:
-          - after: https://github.com/NixOS/nix/pull/8370
-          - need latest static: https://hydra.nixos.org/build/229213111
+      biber217 = {
+        version = "2.17";
+        outPath = builtins.fetchClosure {
+          /* experimental:
+            - after: https://github.com/NixOS/nix/pull/8370
+            - need latest static: https://hydra.nixos.org/build/229213111
 
-          nix profile install \
-            /nix/store/ik8hqwxhj1q9blqf47rp76h7gw7s3060-nix-2.17.1-x86_64-unknown-linux-musl
+            nix profile install \
+              /nix/store/ik8hqwxhj1q9blqf47rp76h7gw7s3060-nix-2.17.1-x86_64-unknown-linux-musl
 
-          - /etc/nix/nix.conf: extra-experimental-features = fetch-closure
-          - systemctl restart nix-daemon.service
-        */
-        inputAddressed = true;
-        fromStore = "https://cache.nixos.org";
-        fromPath = /nix/store/pbv19v0mw57sxa7h6m1hzjvv33mdxxdf-perl5.36.0-biber-2.17;
-        ## ^ from: https://hydra.nixos.org/build/202359527#tabs-details
+            - /etc/nix/nix.conf: extra-experimental-features = fetch-closure
+            - systemctl restart nix-daemon.service
+          */
+          inputAddressed = true;
+          fromStore = "https://cache.nixos.org";
+          fromPath = /nix/store/pbv19v0mw57sxa7h6m1hzjvv33mdxxdf-perl5.36.0-biber-2.17;
+          ## ^ from: https://hydra.nixos.org/build/202359527#tabs-details
+        };
       };
 
     in final: prev: let
