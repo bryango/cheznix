dcf359dcae8a721e98efb4f37be86b6b131f5488
Bryan Lai <bryanlais@gmail.com>
2023-08-19 16:57:40 +0800

    Revamp v2ray-ctrl script

--- modules/v2ray-ctrl/vv.sh
+++ modules/v2ray-ctrl/vv.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # v2ray & cow
-# shellcheck disable=1090,1091,2034
-
-source "$HOME/.bash_utils"
 
 outbounds="@outbounds@"
 routings="@routings@"
@@ -10,58 +7,75 @@ routings="@routings@"
 [[ -n $1 ]] && outbounds=$1 && shift
 [[ ! $# -eq 0 ]] && routings=$1
 
-# shellcheck disable=2154
-init_commands="$set_trap"
+## this is the function that gets passed to tmux
+function core_function {
+    outbounds=$1
+    routings=$2
+
+    set -x
+    trap 'zsh -i; trap - EXIT' EXIT HUP INT QUIT PIPE TERM
+
+    V2RAY_OUTBOUND_CHAIN=$outbounds \
+    V2RAY_ROUTING_SETUP=$routings \
+    v2config > /tmp/v2config.json
+
+    v2ray run -c /tmp/v2config.json \
+        | sed --unbuffered -E 's|v2ray.com/||g' \
+        | sed -E 's/^[0-9]+[^ ]+ //g'
+}
+
+## convert it to string
+export -f core_function
+
+## pass as a string
+core_function=$(printenv BASH_FUNC_core_function%%)
+
+
+function kill_families {
+    read -r -a parent_procs <<< "$(
+        pgrep "$@" | grep -v "^$$$"
+    )"
+    for parent in "${parent_procs[@]}"; do
+        ## kill child processes
+        pkill -9 -P "$parent"
+    done
+    [[ -n ${parent_procs[*]} ]] && kill -9 "${parent_procs[@]}"
+    true
+} &>/dev/null
 
 core_app='v2ray'
 conflicted=(curl-ipinfo sslocal kcptun-client cow "$core_app")
-function core-function {
-    tr -s ' ' <<- EOF
-        V2RAY_OUTBOUND_CHAIN=$outbounds \
-        V2RAY_ROUTING_SETUP=$routings \
-        v2config > /tmp/v2config.json;
-        v2ray run -c /tmp/v2config.json \
-            | sed --unbuffered -E 's|v2ray.com/||g' \
-            | sed -E 's/^[0-9]+[^ ]+ //g'
-EOF
-}
-
-########
 
 >&2 echo "## Cleaning up previous sessions ..."
+{
+    kill_families --full "$(readlink -f "$0")"
+    for proc in "${conflicted[@]}";
+    do
+        kill_families "$proc"
+    done
 
-kill-family "$(readlink -f "$0")" "$$" &>/dev/null || true
-for proc in "${conflicted[@]}";
-do
-    # shellcheck disable=2207
-    IFS=$'\n' procs=( $(pgrep "$proc") )
-    kill -9 "${procs[@]}" &>/dev/null || true
-done
-
-byobu kill-session -t "v2ray"
+    byobu-tmux kill-session -t "v2ray"
+} &>/dev/null
 >&2 echo "## Cleaned up."
 
-byobu new-session -d -s "$core_app" -n "$core_app" \
-    bash -c "$init_commands; $(core-function "$outbounds")"
+
+## set up the byobu session
+byobu-tmux new-session -d -s "$core_app" -n "$core_app" \
+    bash -c "fn $core_function; fn $outbounds $routings"
 
 
 >&2 echo "## Checking connection status ..."
 
-sleep 1  # for the connection to be up
-
-counter=0
-while [[ "$counter" -lt 3 ]]; do
+for _ in {1..10}; do
+    sleep 0.5
     if proxy_status=$(proxychains -q curl-ipinfo); then
         break
-    else
-        counter=$((counter + 1))
-        sleep .5
     fi
 done
 
 proxy_status=$(echo "$proxy_status" \
-    | sed -E -e 's/, /,\n/g' \
-    | awk '!/hostname|loc|postal|phone/'
+    | sed -E 's/, /,\n/g' \
+    | grep -v -E 'hostname|loc|postal|phone'
 )
 [[ -z $proxy_status ]] \
     && proxy_status='Possible Failure! No status returned.' \
@@ -73,3 +87,4 @@ notify-send "$core_app - $outbounds" \
     --hint=int:transient:1 \
 &>/dev/null || true
 
+[[ -n $failure ]] && exit 1 || exit 0
