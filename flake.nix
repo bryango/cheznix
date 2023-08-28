5f02d13da1071d266bf9e84d43ab45366a500025
Bryan Lai <bryanlais@gmail.com>
2023-08-28 22:23:19 +0800

    Revert "Override `buildEnv` ..."
    Overriding `buildEnv` would cause a mass rebuild.
    This reverts commit 8e2d32be4bd08647bedce19bafd81bc6c9ebc305.

--- flake.nix
+++ flake.nix
@@ -69,16 +69,6 @@
           glibcLocales = "/usr";
         };
 
-        ## this causes mass rebuilds
-        buildEnv = attrs: (prev.buildEnv attrs).overrideAttrs (
-          finalAttrs: prevAttrs: {
-            ## blacklist glibcLocales
-            disallowedRequisites = [ final.glibcLocales ] ++ (
-              prevAttrs.disallowedRequisites or []
-            );
-          }
-        );
-
         inherit (system-manager.packages.${prev.system}) system-manager;
 
       };
