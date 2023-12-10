2c5762ebf6a8e016fcdd5080fdca9fffe9f76a14
Bryan Lai <bryanlais@gmail.com>
2023-12-10 14:38:32 +0800

    Use bryango/nixpkgs with `tectonic-with-biber`

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ cd "$(dirname "$0")" || exit
 nix build . "$@"
 code=$?
 
-set +x
-echo "## check consistency of biber217"
+# set +x
+# echo "## check consistency of biber217"
 
-set -x
-src=$(nix eval --raw --no-write-lock-file ./pkgs/tectonic-with-biber#biber)
-out=$(nix eval --raw .#biber217)
+# set -x
+# src=$(nix eval --raw --no-write-lock-file ./pkgs/tectonic-with-biber#biber)
+# out=$(nix eval --raw .#biber217)
 
-set +x
-if [[ "$src" == "$out" ]]
-then echo "## consistent!"
-else echo "## inconsistent!"
-fi
+# set +x
+# if [[ "$src" == "$out" ]]
+# then echo "## consistent!"
+# else echo "## inconsistent!"
+# fi
 
 exit "$code"
--- overlays/mods.nix
+++ overlays/mods.nix
@@ -2,28 +2,6 @@ final: prev:
 
 { ## be careful of `rec`, might not work
 
-  biber217 = let version = "2.17"; in (
-    prev.biber.override {
-      texlive.pkgs.biber.texsource =  {
-        inherit version;
-        inherit (prev.biber) pname meta;
-      };
-    }
-  ).overrideAttrs {
-    src = prev.fetchFromGitHub {
-      owner = "plk";
-      repo = "biber";
-      rev = "v${version}";
-      hash = "sha256-Tt2sN2b2NGxcWyZDj5uXNGC8phJwFRiyH72n3yhFCi0=";
-    };
-    patches = [
-      (prev.fetchpatch {
-        url = "https://patch-diff.githubusercontent.com/raw/plk/biber/pull/411.patch";
-        hash = "sha256-osgldRVfe3jnMSOMnAMQSB0Ymc1s7J6KtM2ig3c93SE=";
-      })
-    ];
-  };
-
   pulsar = prev.pulsar.overrideAttrs (prev: {
     version = "1.110.0";
     src = builtins.fetchClosure {
@@ -38,10 +16,6 @@ final: prev:
       toPath = /nix/store/zs6h3bsw8xmbmxb3rmhqvgqsb9z5szpy-Linux.pulsar-1.110.0.tar.gz;
     };
   });
-
-  tectonic-with-biber = prev.callPackage ../pkgs/tectonic-with-biber {
-    biber = final.biber217;
-  };
 
   /* ## not used by me, disabled to save build time
   fcitx5-configtool =
