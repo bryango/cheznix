4e322eb2d861a98094669a6081a7d66bfab32dc5
Bryan Lai <bryanlais@gmail.com>
2024-01-22 01:06:13 +0800

    Refactor checkpoint build & cutoff infinity

--- overlay.nix
+++ overlay.nix
@@ -6,37 +6,24 @@ final: prev: {
   system-manager-artifacts =
     let
       inherit (final.cheznix.inputs) last-gen;
-      inherit (last-gen.legacyPackages.${prev.system}) system-manager;
+      inherit (last-gen.legacyPackages.${prev.system})
+        system-manager
+        checkpointBuildTools
+        ;
     in
-    system-manager.passthru.checkpointArtifacts or (
-      prev.checkpointBuildTools.prepareCheckpointBuild system-manager
-    );
+      system-manager.passthru.checkpointArtifacts or (
+        checkpointBuildTools.prepareCheckpointBuild system-manager
+      );
 
   ## this system-manager is not wrapped with nix
   system-manager =
     let
-      inherit (prev) mktemp rsync;
-      checkpointArtifacts = final.system-manager-artifacts;
+      system-manager = final.mkCheckpointBuild
+        final.system-manager-unwrapped
+        final.system-manager-artifacts;
     in
     ## provide artifacts for the future
-    final.addCheckpointArtifacts
-      (final.system-manager-unwrapped.overrideAttrs (prevAttrs: {
-        preBuild = (prevAttrs.preBuild or "") + ''
-          set -e
-          sourceDifferencePatchFile=$(${mktemp}/bin/mktemp)
-          diff -ur ${checkpointArtifacts}/sources ./ > "$sourceDifferencePatchFile" || true
-          { shopt -s dotglob; rm -r ./*; } || true
-
-          ${rsync}/bin/rsync \
-            --checksum --times --atimes --chmod=+w \
-            -r ${checkpointArtifacts}/outputs/ .
-          patch -p 1 -i "$sourceDifferencePatchFile" || echo "$sourcePatch"
-          ## ... do not panic when it's unsuccessful
-          ## ... this happens when the source is unchanged
-          rm "$sourceDifferencePatchFile"
-        '';
-      }))
-  ;
+    final.addCheckpointArtifacts system-manager;
 
   neovim = prev.neovim.override { withRuby = false; };
 
