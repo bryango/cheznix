6dcc85dc0dc5bc015580413c0bc121fd36015b2b
Bryan Lai <bryanlais@gmail.com>
2024-08-06 16:01:54 +0800

    Try automating pulsar-from-ci

--- pkgs/pulsar-from-ci.nix
+++ pkgs/pulsar-from-ci.nix
@@ -1,32 +1,44 @@
 /** pulsar from ci builds, instead of github releases */
 
-{ pulsar }:
+{ pulsar, fetchurl, lib }:
+
+let
+
+  version = "1.119.0";
+
+in
 
 pulsar.overrideAttrs {
-  version = "1.119.0";
+  inherit version;
   src =
-    let
-      /**
-        Pulsar follows a semi-automated release process. Look under github
-        actions for the [artifact] corresponding to the release commit.
-        nightly.link is a helpful service for concurrent downloads.
+    # let
+    #   /**
+    #     Pulsar follows a semi-automated release process. Look under github
+    #     actions for the [artifact] corresponding to the release commit.
+    #     nightly.link is a helpful service for concurrent downloads.
       
-        [artifact]: https://nightly.link/pulsar-edit/pulsar/actions/runs/9949008125
+    #     [artifact]: https://nightly.link/pulsar-edit/pulsar/actions/runs/9949008125
 
-        - nix store add-path # NOT add-file, different hashing scheme
-        - cachix push chezbryan
-        - cachix pin chezbryan pulsar-source
-        - nix store make-content-addressed # check that hash is _not_ changed
+    #     - nix store add-path # NOT add-file, different hashing scheme
+    #     - cachix push chezbryan
+    #     - cachix pin chezbryan pulsar-source
+    #     - nix store make-content-addressed # check that hash is _not_ changed
 
-        See also: https://github.com/NixOS/nix/issues/6210#issuecomment-1060834892
-      */
-      path = /nix/store/rvvbgyfk49axxlgvd9hxgi1jgkppanv6-Linux.pulsar-1.119.0.tar.gz;
-    in
-    builtins.fetchClosure {
-      fromStore = "https://chezbryan.cachix.org";
-      /** it seems that cachix doesn't advertise ca-derivations;
-              no worries, just treat them as input addressed: */
-      toPath = path;
-      fromPath = path;
+    #     See also: https://github.com/NixOS/nix/issues/6210#issuecomment-1060834892
+    #   */
+    #   path = /nix/store/rvvbgyfk49axxlgvd9hxgi1jgkppanv6-Linux.pulsar-1.119.0.tar.gz;
+    # in
+    # builtins.fetchClosure {
+    #   fromStore = "https://chezbryan.cachix.org";
+    #   /** it seems that cachix doesn't advertise ca-derivations;
+    #           no worries, just treat them as input addressed: */
+    #   toPath = path;
+    #   fromPath = path;
+    # };
+    fetchurl {
+      url = "https://github.com/pulsar-edit/pulsar/actions/runs/9949008125/artifacts/1704337116";
+      hash = lib.fakeHash;
+      pname = "pulsar-ubuntu";
+      inherit version;
     };
 }
