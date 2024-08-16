f98a379097ed04fb3ee7d0bb9246fa87147a4389
Bryan Lai <bryanlais@gmail.com>
2024-08-16 14:18:19 +0800

    Test home-manager with PR 5738

--- overlays/hm.nix
+++ overlays/hm.nix
@@ -3,8 +3,14 @@
 final: prev:
 
 let
-  inherit (prev) home-manager;
-  inherit (home-manager) src;
+  inherit (prev) home-manager fetchFromGitHub;
+  src = fetchFromGitHub {
+    name = "home-manager-source";
+    owner = "nix-community";
+    repo = "home-manager";
+    rev = "97069e11282bd586fb46e3fa4324d651668e1886";
+    hash = "sha256-otzfwp5EpQ6mlnASeGu0uSKoatlQnBvLv/f4JP+WtfA=";
+  };
 in
 {
   home-manager = home-manager.overrideAttrs (finalAttrs: prevAttrs:
@@ -13,6 +19,7 @@ in
       inherit (import "${src}/flake.nix") outputs;
     in
     {
+      inherit src;
       passthru = prevAttrs.passthru // {
         flake = (outputs {
           self = finalAttrs.passthru.flake;
