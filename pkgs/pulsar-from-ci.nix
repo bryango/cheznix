b2fd3d048c165a5a2701dd1315fb9f21e34d442f
Bryan Lai <bryanlais@gmail.com>
2024-08-20 10:42:20 +0800

    Bump pulsar to 1.120

--- pkgs/pulsar-from-ci.nix
+++ pkgs/pulsar-from-ci.nix
@@ -7,7 +7,7 @@
   actions for the [artifact] corresponding to the release commit.
   nightly.link is a helpful service for concurrent downloads.
 
-  [artifact]: https://nightly.link/pulsar-edit/pulsar/actions/runs/9949008125
+  [artifact]: https://nightly.link/pulsar-edit/pulsar/actions/runs/10413849888
 
   To include the build artifact in nix store and binary cache,
 
@@ -41,65 +41,65 @@ let
 
   /** id of the linux artifact from the github action,
       obtained from the download link through the web ui */
-  artifact_id = "1704337116";
-  hash = "sha256-2Zg44mdRdnMAuTBmlkHhRIWm4KtbV2JuDlFVRabE5QQ=";
+  artifact_id = "1818489884";
+  hash = "sha256-tXjjAE1vVj4UrAHKVsRftNBJlJuF5L3hB9KzEM7hxBM=";
 
 in
 
 pulsar.overrideAttrs (final: prev: {
-  version = "1.119.0";
+  version = "1.120.0";
   src = (fetchzip {
 
-      url = "https://api.github.com/repos/pulsar-edit/pulsar/actions/artifacts/${artifact_id}/zip";
-      extension = "zip";
-      stripRoot = false;
+    url = "https://api.github.com/repos/pulsar-edit/pulsar/actions/artifacts/${artifact_id}/zip";
+    extension = "zip";
+    stripRoot = false;
 
-      /** pick out the `.tar.gz` file */
-      postFetch = ''
-        mv "$out" "$unpackDir"
-        mv "$unpackDir"/*.tar.gz "$out"
-      '';
-      name = "Linux.pulsar-${final.version}.tar.gz";
-      inherit hash;
+    /** pick out the `.tar.gz` file */
+    postFetch = ''
+      mv "$out" "$unpackDir"
+      mv "$unpackDir"/*.tar.gz "$out"
+    '';
+    name = "Linux.pulsar-${final.version}.tar.gz";
+    inherit hash;
 
-      /**
-        `netrcImpureEnvVars` is appended to the usual `impureEnvVars`.
-        We still need to correctly pass in the environment variables. Note
-        that for a multi-user setup, this must happen on the nix-daemon side,
-        which is probably started as a systemd service, so one needs to
-        correctly patch the service definitions.
-        
-        Alternatively, we may use the `configurable-impure-env` experimental
-        feature, which can be fully specified in `/etc/nix/nix.conf`:
+    /**
+      `netrcImpureEnvVars` is appended to the usual `impureEnvVars`.
+      We still need to correctly pass in the environment variables. Note
+      that for a multi-user setup, this must happen on the nix-daemon side,
+      which is probably started as a systemd service, so one needs to
+      correctly patch the service definitions.
+      
+      Alternatively, we may use the `configurable-impure-env` experimental
+      feature, which can be fully specified in `/etc/nix/nix.conf`:
 
-        ```conf
-          extra-experimental-features = configurable-impure-env
-          impure-env = GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}
-          trusted-users = root runner @wheel
-        ```
+      ```conf
+        extra-experimental-features = configurable-impure-env
+        impure-env = GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}
+        trusted-users = root runner @wheel
+      ```
 
-        This is implemented in: ../.github/workflows/build.yml
+      This is implemented in: ../.github/workflows/build.yml
+    */
+    netrcImpureEnvVars = ["GITHUB_TOKEN"];
+  }).overrideAttrs ({ postFetch, ... }: {
+    /**
+      `postHook` is a seemingly undocumented hook executed at the end of
+      `setupPhase`, as implemented in `setup.sh`. This happens before the
+      actual build process. Here we use it to pass in the actual token.
+
+      We cannot supply `curlOptsList` in nix, as it would be over-escaped
+      and the `$GITHUB_TOKEN` would not be correctly expanded.
       */
-      netrcImpureEnvVars = ["GITHUB_TOKEN"];
-    }).overrideAttrs ({ postFetch, ... }: {
-      /**
-        `postHook` is a seemingly undocumented hook executed at the end of
-        `setupPhase`, as implemented in `setup.sh`. This happens before the
-        actual build process. Here we use it to pass in the actual token.
+    postHook = ''
+      curlOptsList="'--header' 'Authorization: Bearer $GITHUB_TOKEN'"
+    '';
 
-        We cannot supply `curlOptsList` in nix, as it would be over-escaped
-        and the `$GITHUB_TOKEN` would not be correctly expanded.
-       */
-      postHook = ''
-        curlOptsList="'--header' 'Authorization: Bearer $GITHUB_TOKEN'"
-      '';
-
-      /**
-        `postFetch` of `fetchurl` is augmented by `fetchzip`. We patch the
-        final version again to remove the unnecessary executable bits.
-      */
-      postFetch = postFetch + ''
-        chmod 644 "$out"
-      '';
-    });
+    /**
+      `postFetch` of `fetchurl` is augmented by `fetchzip`. We patch the
+      final version again to remove the unnecessary executable bits.
+    */
+    postFetch = postFetch + ''
+      chmod 644 "$out"
+    '';
+  });
 })
