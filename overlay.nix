9d5acc92e6947e241833bcb68733b76acba92126
Bryan Lai <bryanlais@gmail.com>
2024-09-25 11:20:26 +0800

    Update home-manager with patch

--- overlay.nix
+++ overlay.nix
@@ -6,17 +6,17 @@ final: prev: with prev; {
     ## option inspection does not work for flakes
     ## so simply drop this dependency to save space
     nixos-option = null;
-    /** use GNU's hostname as it's more powerful */
-    unixtools.hostname = inetutils;
+    # /** use GNU's hostname as it's more powerful */
+    # unixtools.hostname = inetutils;
   }).overrideAttrs {
     /** fix hostname issues */
-    src = fetchFromGitHub {
-      name = "home-manager-source";
-      owner = "nix-community";
-      repo = "home-manager";
-      rev = "97069e11282bd586fb46e3fa4324d651668e1886";
-      hash = "sha256-otzfwp5EpQ6mlnASeGu0uSKoatlQnBvLv/f4JP+WtfA=";
-    };
+    # src = fetchFromGitHub {
+    #   name = "home-manager-source";
+    #   owner = "nix-community";
+    #   repo = "home-manager";
+    #   rev = "97069e11282bd586fb46e3fa4324d651668e1886";
+    #   hash = "sha256-otzfwp5EpQ6mlnASeGu0uSKoatlQnBvLv/f4JP+WtfA=";
+    # };
 
   };
 
