8bb2ecf2c3d5005e5a06acd53a0939f7a1ce6ae6
Bryan Lai <bryanlais@gmail.com>
2024-12-24 23:52:03 +0800

    Improve pr-checker.sh

--- pr-checker.sh
+++ pr-checker.sh
@@ -3,25 +3,35 @@
 
 set -e
 
+REPO=NixOS/nixpkgs
+BASE_URL=https://github.com
+REPO_URL="$BASE_URL/$REPO"
+
 prNumber=$1
 trackedBranch=$2
 
 [[ -n $trackedBranch ]] || trackedBranch="nixpkgs-unstable"
+(
+  targetHash=$(gh api "/repos/$REPO/commits/$trackedBranch" --jq .sha)
+  [[ "$trackedBranch" != "$targetHash" ]] && >&2 echo "# $trackedBranch: $REPO_URL/commit/$targetHash"
+) &
 
 if [[ -z $prNumber ]]; then
-  cmd='gh --repo NixOS/nixpkgs pr list --author "@me" --state merged --limit 10'
+  cmd="gh --repo \"$REPO\" pr list --author \"@me\" --state merged --limit 10"
   >&2 echo "# re-run and specify a PR to check, e.g."
   >&2 echo "$ $cmd"
   eval "$cmd"
   exit
 fi
 
-if commitHash=$(gh --repo NixOS/nixpkgs pr view --json mergeCommit --jq .mergeCommit.oid "$prNumber");
+if commitHash=$(gh --repo "$REPO" pr view --json mergeCommit --jq .mergeCommit.oid "$prNumber");
 then :; else
   commitHash=$prNumber # allow using plain hash
 fi
-compareLink="NixOS/nixpkgs/compare/$commitHash...$trackedBranch"
+compareLink="$REPO/compare/$commitHash...$trackedBranch"
 
->&2 echo "# checking PR $prNumber: https://github.com/$compareLink"
+>&2 echo "# checking $prNumber: $BASE_URL/$compareLink"
 
 gh api "repos/$compareLink?per_page=1000000&page=100" --jq .status
+
+wait # for the background process
