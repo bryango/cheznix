97ed64c9027912f6f91fddea0843fa869e78a258
Bryan Lai <bryanlais@gmail.com>
2025-05-23 15:54:28 +0800

    feat(darwin): cp darwin apps to ~/Applications

--- modules/home-setup.nix
+++ modules/home-setup.nix
@@ -52,6 +52,29 @@ in
 
     };
 
+    # just copy instead of linking darwin apps
+    targets.darwin.linkApps.enable = false;
+    home.activation.copyDarwinApps = lib.mkIf pkgs.stdenv.isDarwin (
+      let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        baseDir="${config.targets.darwin.linkApps.directory}"
+        if [ -d "$baseDir" ]; then
+          rm -rf "$baseDir"
+        fi
+        mkdir -p "$baseDir"
+        for appFile in ${apps}/Applications/*; do
+          target="$baseDir/$(basename "$appFile")"
+          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+        done
+      ''
+    );
+
     nix.extraOptions = ''
 
       ## ... config follows from `/etc/nix/nix.conf`
