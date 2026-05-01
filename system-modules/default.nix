c151542441dc8d81f7d3ac34aae3b9b5d541a3a5
Bryan Lai <bryanlais@gmail.com>
2026-05-01 15:52:27 +0800

    feat(system/zsh): enable zsh plugins

--- system-modules/default.nix
+++ system-modules/default.nix
@@ -7,7 +7,9 @@
 }:
 
 let
-  /** modules migrated from nixpkgs upstream */
+  /**
+    modules migrated from nixpkgs upstream
+  */
   upstreamModules = (cheznix.inputs.system-manager.outPath + "/nix/modules/upstream/nixpkgs");
 in
 {
@@ -25,12 +27,63 @@ in
   ++ map (path: nixosModulesPath + path) [
     ## NixOS modules, with a leading "/"
     "/programs/zsh/zsh.nix"
+    "/programs/zsh/oh-my-zsh.nix"
+    "/programs/zsh/zsh-autosuggestions.nix"
+    "/programs/zsh/zsh-syntax-highlighting.nix"
   ];
 
   config = {
-    programs.zsh.enable = true;
     system-manager.allowAnyDistro = true;
     nixpkgs.hostPlatform = pkgs.stdenv.hostPlatform.system;
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableBashCompletion = true;
+      enableLsColors = true;
+      autosuggestions.enable = true;
+      ohMyZsh.enable = true;
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [
+          "main"
+          "brackets"
+          "pattern"
+          "root"
+          "line"
+        ];
+        styles.line = "bold";
+        # styles.root = "";
+      };
+      histSize = 1000000;
+      setOptions = [
+        # same background '&' as in bash
+        "NO_HUP"
+        "NO_CHECK_JOBS"
+
+        # trim history
+        "HIST_FIND_NO_DUPS"
+        "HIST_IGNORE_DUPS"
+      ];
+      # PROMPT=$'\n%B> %* %n@%F{yellow}%m%f%b %~ %(?..%F{red}!%?)%f%b\n%(!.#.$) '
+      # PROMPT=$'\n%B%(!.%F{red}.%F{green})> %* %n@%F{yello}%m%(!.%F{red}.%F{green})%b %~ %(?..%F{red}!%?)%f%b\n%(!.#.$) '
+      promptInit = ''
+        PROMPT=$'\n%B> %* %(!.%F{red}.%F{green})%n%f@%F{yello}%m%f%b %~ %B%(?..%F{red}!%?)%f%b\n%(!.#.$) '
+        setopt prompt_sp
+      '';
+      interactiveShellInit = ''
+        export WORDCHARS='|'
+        bindkey '' undo
+
+        # ls when pwd changed
+        chpwd() {
+            ls --color=auto -alhF --group-directories-first >&2
+            >&2 echo "$(tput bold)>> $PWD$(tput sgr0)"
+        }
+
+        # make available for other shells
+        export FPATH
+      '';
+    };
 
     # environment.systemPackages = with pkgs; [
     #   zsh
