072d45b0d1ca4e2d1c917e9ecac160852207cd13
Bryan Lai <bryanlais@gmail.com>
2026-06-10 00:40:00 +0800

    feat(ci): simplify config build script

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 set -x
 cd "$(dirname "$0")" || exit
 
-# same command for `home-manager` and `darwin-rebuild`
-# see: packages.${system}.default
-exec nix run . -- build -v "$@"
+# # same command for `home-manager` and `darwin-rebuild`
+# # see: packages.${system}.default
+# exec nix run . -- build -v "$@"
+
+exec nix build .# "$@"
